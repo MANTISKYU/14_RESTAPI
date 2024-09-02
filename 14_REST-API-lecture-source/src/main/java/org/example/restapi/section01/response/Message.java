dbb81d0ff0933a10db866f4eff5f23cb2a599223
이득규 <asdaron44@gmail.com>
2024-09-02 10:44:19 +0900

    first commit

---
+++ 14_REST-API-lecture-source/src/main/java/org/example/restapi/section01/response/Message.java
@@ -0,0 +1,39 @@
+package org.example.restapi.section01.response;
+
+public class Message {
+
+    private int httpStatusCode;
+    private String message;
+
+    public Message() {
+    }
+
+    public int getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    public void setHttpStatusCode(int httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Message(int httpStatusCode, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "httpStatusCode=" + httpStatusCode +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
